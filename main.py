e866e99aa4e00f854779cb8422cac5c742d00dd9
Louis Munier <louis.munier@epfl.ch>
2019-04-24 13:00:14 +0200

    Update storing info and fix issues with displaying information.

--- main.py
+++ main.py
@@ -14,37 +14,49 @@ __version__ = '0.1'
 
 
 class Team:
-    def __init__(self, team, _ski, _tchoukball, _gym):
+    def __init__(self, folder, team):
         """Function to initialize a member of the class player."""
 
         # Register team name
         self.team_name = team
 
-        # Register state of each sport
-        self.state_ski = _ski
-        self.state_tchoukball = _tchoukball
-        self.state_gym = _gym
+        with open(folder + '/{}.txt'.format(self.team_name.replace(' ', '_')), 'r') as file:
+            # read a list of lines into data
+            data = file.readlines()
 
-        # Register score of each sport
-        self.score_ski = 0
-        self.score_tchoukball = 0
-        self.score_gym = 0
-        self.score_total = 0
+            # Recover state of each sport
+            self.state_ski = int(data[ski].split(' ')[0])
+            self.state_tchoukball = int(data[tchoukball].split(' ')[0])
+            self.state_gym = int(data[gym].split(' ')[0])
 
-    def update_score(self, folder, discipline_idx, score, time):
+            # Recover score of each sport
+            self.score_ski = int(data[ski].split(' ')[1])
+            self.score_tchoukball = int(data[tchoukball].split(' ')[1])
+            self.score_gym = int(data[gym].split(' ')[1])
+            self.score_total = int(data[3])
+
+            # Recover time of each sport
+            self.time_ski = data[ski].split(' ')[2]
+            self.time_tchoukball = data[tchoukball].split(' ')[2]
+            self.time_gym = data[gym].split(' ')[2]
+
+    def update_team(self, folder, discipline_idx, score, time='-:--:--'):
         """Update the score for a specific player and store it."""
 
         if discipline_idx == ski:
             self.score_ski = score
+            self.time_ski = time
         elif discipline_idx == tchoukball:
             self.score_tchoukball = score
+            self.time_tchoukball = time
         elif discipline_idx == gym:
             self.score_gym = score
+            self.time_gym = time
 
         self.score_total = self.score_ski + self.score_tchoukball + self.score_gym
         self.update_file(folder, discipline_idx, score, time)
 
-    def update_file(self, folder, discipline_idx, score, time):
+    def update_file(self, folder, discipline_idx, score, time='-:--:--'):
         """Update the file of a specific player to have a backup."""
 
         # Recover all the information in the file
@@ -111,25 +123,25 @@ class DisplayWindow(QMainWindow, int_display.Ui_MainWindow):
         self.label_time_tchoukball.setText('-:--:--')
         self.label_time_gym.setText('-:--:--')
 
-        # Reinitialize team name
-        self.label_team_ski.setText('team')
-        self.label_team_tchoukball.setText('team')
-        self.label_team_gym.setText('team')
+        # Reinitialize score
+        self.nb_points_ski.setText('0')
+        self.nb_points_tchoukball.setText('0')
+        self.nb_points_gym.setText('0')
 
     def fige_time(self, discipline_idx):
         """Fige and display time for a specified discipline."""
 
-        min = str(int(self.lcd_general_min.value()))
+        minute = str(int(self.lcd_general_min.value()))
         sec = str(int(self.lcd_general_sec.value()))
         millis = str(int(self.lcd_general_millis.value()))
 
         # Display time DisplayWindow
         if discipline_idx == ski:
-            self.label_time_ski.setText(min + ':' + sec + ':' + millis)
+            self.label_time_ski.setText(minute + ':' + sec + ':' + millis)
         elif discipline_idx == tchoukball:
-            self.label_time_tchoukball.setText(min + ':' + sec + ':' + millis)
+            self.label_time_tchoukball.setText(minute + ':' + sec + ':' + millis)
         elif discipline_idx == gym:
-            self.label_time_gym.setText(min + ':' + sec + ':' + millis)
+            self.label_time_gym.setText(minute + ':' + sec + ':' + millis)
 
     def update_timer(self):
         """Update timer calling by an other timer and update with time elapsed from the beginning."""
@@ -137,11 +149,11 @@ class DisplayWindow(QMainWindow, int_display.Ui_MainWindow):
         if self.started:
             elapsed_time = self.time_elapsed.elapsed()
 
-            min = int(elapsed_time / 60000)
-            sec = int(elapsed_time / 1000 - min * 60)
-            millis = int((elapsed_time - 60000 * min - 1000 * sec) / 10)
+            minute = int(elapsed_time / 60000)
+            sec = int(elapsed_time / 1000 - minute * 60)
+            millis = int((elapsed_time - 60000 * minute - 1000 * sec) / 10)
 
-            self.lcd_general_min.display(min)
+            self.lcd_general_min.display(minute)
             self.lcd_general_sec.display(sec)
             self.lcd_general_millis.display(millis)
 
@@ -183,12 +195,19 @@ class CommandWindow(QMainWindow, int_command.Ui_Form):
         self.button_refresh.clicked.connect(self.refresh)
         self.button_start_all.clicked.connect(self.start_all)
 
+        # Connect label clicked to function fige_time
+        self.button_ski.clicked.connect(lambda: self.fige_time(ski, self.display_window.name_team_ski))
+        self.button_tchoukball.clicked.connect(lambda: self.fige_time(tchoukball,
+                                               self.display_window.name_team_tchoukball))
+        self.button_gym.clicked.connect(lambda: self.fige_time(gym, self.display_window.name_team_gym))
+
         # Connect slider
         self.slider_ski.valueChanged.connect(
             lambda: self.update_score(ski, self.slider_ski.value(), self.display_window.name_team_ski,
                                       self.display_window.nb_points_ski, self.label_score_ski))
         self.slider_tchoukball.valueChanged.connect(
-            lambda: self.update_score(tchoukball, self.slider_tchoukball.value(), self.display_window.name_team_tchoukball,
+            lambda: self.update_score(tchoukball, self.slider_tchoukball.value(),
+                                      self.display_window.name_team_tchoukball,
                                       self.display_window.nb_points_tchoukball, self.label_score_tchoukball))
         self.slider_gym.valueChanged.connect(
             lambda: self.update_score(gym, self.slider_gym.value(), self.display_window.name_team_gym,
@@ -218,17 +237,22 @@ class CommandWindow(QMainWindow, int_command.Ui_Form):
     def refresh(self):
         """Refresh the total interface to have it as the initialization step."""
 
-        # Set started bool value to update vlues
-        self.started = True
-        self.display_window.started = True
+        # Reinitialize started bool value
+        self.started = False
+        self.display_window.started = False
 
-        # Reinitialize diasplay window
+        # Reinitialize display window
         self.display_window.refresh()
 
         # Reinitialize sliders
         self.slider_ski.setValue(0)
         self.slider_tchoukball.setValue(0)
         self.slider_gym.setValue(0)
+
+        # Reinitialize score
+        self.label_score_ski.setText('0')
+        self.label_score_tchoukball.setText('0')
+        self.label_score_gym.setText('0')
 
         # Reinitialize timer
         self.lcd_min.display(0)
@@ -242,10 +266,6 @@ class CommandWindow(QMainWindow, int_command.Ui_Form):
 
         self.time_elapsed.restart()
 
-        # Reinitialize started bool value
-        self.started = False
-        self.display_window.started = False
-
         # Deactivate sliders
         self.slider_ski.setEnabled(self.started)
         self.slider_tchoukball.setEnabled(self.started)
@@ -255,13 +275,45 @@ class CommandWindow(QMainWindow, int_command.Ui_Form):
         """Display the previous match."""
 
         self.match_state = (self.match_state - 1) % self.nb_match
-        print(self.match_state)
+        self.update_match()
 
     def next_match(self):
         """Display the next match."""
 
         self.match_state = (self.match_state + 1) % self.nb_match
-        print(self.match_state)
+        self.update_match()
+
+    def update_match(self):
+        """Update match and display all information."""
+
+        with open(self.folder_storage + "/match.txt", "r") as match_file:
+            match = match_file.readlines()[self.match_state].split(',')
+
+            self.display_window.name_team_ski.setText(match[0])
+            self.display_window.name_team_tchoukball.setText(match[1])
+            self.display_window.name_team_gym.setText(match[2][:-1])
+
+        for t in self.dict_teams.values():
+            if t.team_name == self.display_window.name_team_ski.text():
+                self.display_window.label_time_ski.setText(str(t.time_ski))
+                self.display_window.nb_points_ski.setText(str(t.score_ski))
+            elif self.display_window.name_team_ski.text() == '':
+                self.display_window.label_time_ski.setText('-:--:--')
+                self.display_window.nb_points_ski.setText('0')
+
+            if t.team_name == self.display_window.name_team_tchoukball.text():
+                self.display_window.label_time_tchoukball.setText(str(t.time_tchoukball))
+                self.display_window.nb_points_tchoukball.setText(str(t.score_tchoukball))
+            elif self.display_window.name_team_tchoukball.text() == '':
+                self.display_window.label_time_tchoukball.setText('-:--:--')
+                self.display_window.nb_points_tchoukball.setText('0')
+
+            if t.team_name == self.display_window.name_team_gym.text():
+                self.display_window.label_time_gym.setText(str(t.time_gym))
+                self.display_window.nb_points_gym.setText(str(t.score_gym))
+            elif self.display_window.name_team_gym.text() == '':
+                self.display_window.label_time_gym.setText('-:--:--')
+                self.display_window.nb_points_gym.setText('0')
 
     def update_score(self, discipline_idx, slider_value, disp_label_team, disp_score, command_score):
         """ Update score for a specific team."""
@@ -276,7 +328,7 @@ class CommandWindow(QMainWindow, int_command.Ui_Form):
 
             # Recover time figed if slider_value = max_slider_value
             if slider_value == self.max_slider_value:
-                time = self.fige_time(discipline_idx)
+                time = self.fige_time(discipline_idx, disp_label_team)
 
             # Display score CommandWindow
             command_score.setText(str(score))
@@ -286,29 +338,40 @@ class CommandWindow(QMainWindow, int_command.Ui_Form):
 
             # Update score in class and in file
             for t in self.dict_teams.values():
-                if 'team' == disp_label_team.text():
-                    t.update_score(self.folder_storage, discipline_idx, score, time)
+                if t.team_name == disp_label_team.text():
+                    t.update_team(self.folder_storage, discipline_idx, score, time)
 
-    def fige_time(self, discipline_idx):
+    def fige_time(self, discipline_idx, disp_label_team):
         """Fige and display time for a specified discipline."""
-        min, sec, millis = '', '', ''
+        score = 0
+        minute, sec, millis = '', '', ''
 
-        if self.started:
-            min = str(int(self.lcd_min.value()))
+        if self.started and disp_label_team.text() != '':
+            minute = str(int(self.lcd_min.value()))
             sec = str(int(self.lcd_sec.value()))
             millis = str(int(self.lcd_millis.value()))
 
-            # Display time CommandWindow
+            time = minute + ':' + sec + ':' + millis
+
+            # Display time CommandWindow and DisplayWindow
             if discipline_idx == ski:
-                self.label_time_ski.setText(min + ':' + sec + ':' + millis)
+                self.label_time_ski.setText(time)
+                score = int(self.label_score_ski.text())
             elif discipline_idx == tchoukball:
-                self.label_time_tchoukball.setText(min + ':' + sec + ':' + millis)
+                self.label_time_tchoukball.setText(time)
+                score = int(self.label_score_tchoukball.text())
             elif discipline_idx == gym:
-                self.label_time_gym.setText(min + ':' + sec + ':' + millis)
+                self.label_time_gym.setText(time)
+                score = int(self.label_score_gym.text())
 
             self.display_window.fige_time(discipline_idx)
 
-        return min + ':' + sec + ':' + millis
+            # Update time in class and in file
+            for t in self.dict_teams.values():
+                if t.team_name == disp_label_team.text():
+                    t.update_team(self.folder_storage, discipline_idx, score, time)
+
+        return minute + ':' + sec + ':' + millis
 
     def update_timer(self):
         """Update timer calling by an other timer and update with time elapsed from the beginning."""
@@ -316,11 +379,11 @@ class CommandWindow(QMainWindow, int_command.Ui_Form):
         if self.started:
             elapsed_time = self.time_elapsed.elapsed()
 
-            min = int(elapsed_time / 60000)
-            sec = int(elapsed_time / 1000 - min * 60)
-            millis = int((elapsed_time - 60000 * min - 1000 * sec) / 10)
+            minute = int(elapsed_time / 60000)
+            sec = int(elapsed_time / 1000 - minute * 60)
+            millis = int((elapsed_time - 60000 * minute - 1000 * sec) / 10)
 
-            self.lcd_min.display(min)
+            self.lcd_min.display(minute)
             self.lcd_sec.display(sec)
             self.lcd_millis.display(millis)
 
@@ -354,25 +417,32 @@ def init_team_files(folder):
                 with open(folder + '/{}.txt'.format(name[:-1]), "a") as fh:
                     fh.write('0')
 
-            dictionary_teams[name[:-1]] = Team(name[:-1].replace('_', ' '), int(info_team[-3]),
-                                               int(info_team[-2]), int(info_team[-1]))
+            dictionary_teams[name[:-1]] = Team(folder, name[:-1].replace('_', ' '))
 
     return dictionary_teams
 
 
-def init_match_file(folder, dict_teams):
+def init_match_file(folder, dictionary_teams):
     """Initialization function to init file for each match."""
 
     # Fill list to recover all the team participating in each sports
+    empty_lists = True
     list_sports = [[], [], []]
 
-    for t in dict_teams.values():
+    for t in dictionary_teams.values():
         if t.state_ski == 1:
             list_sports[ski].append(t.team_name)
         if t.state_tchoukball == 1:
             list_sports[tchoukball].append(t.team_name)
         if t.state_gym == 1:
             list_sports[gym].append(t.team_name)
+
+    for l in list_sports:
+        if l:
+            empty_lists = False
+
+    if empty_lists:
+        return None
 
     # Find the longest list to fill the other ones with blank
     longest_list = max(list_sports, key=len)
@@ -416,15 +486,18 @@ def init_match_file(folder, dict_teams):
 
             match += list_tchoukball[0] + ','
 
-            while list_gym[0] == m or list_gym[0] == list_tchoukball[0]:
-                j += 1
+            if list_gym[0] != m and list_gym[0] != list_tchoukball[0]:
+                solution = True
+            else:
+                while list_gym[0] == m or list_gym[0] == list_tchoukball[0]:
+                    j += 1
 
-                if j == 20:
-                    solution = False
-                    break
-                else:
-                    random.shuffle(list_gym)
-                    solution = True
+                    if j == 20:
+                        solution = False
+                        break
+                    else:
+                        random.shuffle(list_gym)
+                        solution = True
 
             list_match.append(match + list_gym[0] + '\n')
 
@@ -447,7 +520,11 @@ if __name__ == "__main__":
     # Init team and match files
     folder_name = "team_match"
     dict_teams = init_team_files(folder_name)
+
     nb_match = init_match_file(folder_name, dict_teams)
+
+    if nb_match is None:
+        sys.exit(app.exit())
 
     # Init and start timer
     start_timer = QtCore.QElapsedTimer()
